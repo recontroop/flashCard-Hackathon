3672875e43edc00484d555a4662a5e7b7c20997c
Rusbeh <rusbeh.turki@gmail.com>
2017-04-23 00:17:13 -0400

    test2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//
+
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
@@ -10,13 +10,21 @@ var url = 'mongodb://rusbeh:rusbeh42@flashme-shard-00-00-ml985.mongodb.net:27017
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
-  alert("hello");
+
   db.close();
 });
 
-
-//
-/*
-  var userName = document.getElementById("userName")
-  var password = document.getElementById("password")
-*/
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
