bce29f60f90d0f6abba64a3edfebbc7078933a89
Rusbeh <rusbeh.turki@gmail.com>
2017-04-23 01:01:16 -0400

    FUCK
    SHIT

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ var MongoClient = require('mongodb').MongoClient
 // Connection URL
 var url = 'mongodb://rusbeh:rusbeh42@flashme-shard-00-00-ml985.mongodb.net:27017,flashme-shard-00-01-ml985.mongodb.net:27017,flashme-shard-00-02-ml985.mongodb.net:27017/flashMe?ssl=true&replicaSet=flashMe-shard-0&authSource=admin';
 
-
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
-  db.close();
+  insertDocuments(db, function() {
+    db.close();
+  });
 });
-
+//-------------------------------------------------------
 var insertDocuments = function(db, callback) {
   // Get the documents collection
   var collection = db.collection('documents');
@@ -28,3 +31,4 @@ var insertDocuments = function(db, callback) {
     callback(result);
   });
 }
+//-------------------------------------------------------
